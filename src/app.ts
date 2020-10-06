6db24de23b36b615cd4f30a7dc9bb10b1fce542c
Pontus N <pontusnorr@gmail.com>
2020-10-06 15:16:29 +0200

    Added db example

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,55 @@
+import bodyParser from 'body-parser';
 import express from "express";
+import path from 'path';
+import session from 'express-session';
+import sqlite3 from "sqlite3"
 
 const app = express();
 const port = 8080;
 
-app.set("view engine", "ejs")
+const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READONLY, err => {
+    if (err)
+        console.error(err);
+});
+
+app.set("view engine", "ejs");
+app.use(session({
+    resave: true,
+    saveUnitialized: true,
+    secret: "secret"
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+
 app.get("/", (_, res) => {
-    res.render("pages/home");
+    res.render("pages/login");
+});
+app.post("/auth", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        console.log("wot");
+        db.get(`SELECT * FROM users
+        WHERE username = ?
+        AND password = ?`,
+        [username, password],
+        (err, row) => {
+            if (err) {
+                console.log("oof");
+            } else if (!row) {
+                console.log("Could not find user " + username);
+            } else {
+                console.log(row.id + " : " + row.username);
+
+            }
+        });
+    }
+    res.send("oop");
+    res.end();
 });
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
 });
-
-
