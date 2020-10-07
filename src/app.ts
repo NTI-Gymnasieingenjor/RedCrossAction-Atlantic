3857630e5bdcb005cde1d7d2e0b6c0c0a0026774
Pluppen <eliass.juremalm@gmail.com>
2020-10-07 13:15:21 +0200

    Add routes and login

--- src/app.ts
+++ src/app.ts
@@ -14,71 +14,84 @@ const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READONLY, err => {
 
 app.set("view engine", "ejs");
 app.use(express.static('public'))
+
 app.use(session({
-    resave: true,
-    saveUnitialized: true,
+    resave: false,
+    saveUninitialized: false,
     secret: "secret"
 }));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-
 app.get("/", (_, res) => {
     res.render("pages/login");
 });
+
 app.post("/auth", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
     if (username && password) {
-        console.log("wot");
         db.get(`SELECT * FROM users
         WHERE username = ?
         AND password = ?`,
         [username, password],
         (err, row) => {
             if (err) {
-                console.log("oof");
+                console.log(err);
+                res.render("pages/login", {data: {error_msg: "An error occured."}});
             } else if (!row) {
                 console.log("Could not find user " + username);
+                res.render("pages/login", {data: {error_msg: "Wrong username or password."}})
             } else {
                 console.log(row.id + " : " + row.username);
-
+                req.session.role = "jour";
+                res.redirect("dashboard");
             }
         });
     }
-    res.send("oop");
-    res.end();
 });
 
 app.get("/dashboard", (req, res) => {
-    res.render("pages/dashboard");
+    if(req.session.role === "jour"){
+        res.render("pages/dashboard");
+    } else {
+        res.redirect("/");
+    }
 });
 
 app.get("/signup", (req, res) => {
     res.render("pages/signup");
 });
 
+app.get("/moreinfo", (req, res) => {
+    res.render("pages/more_info");
+});
+
 // Temp array to keep ids who opened link.
-let usersAnswered = [];
+let usersAnsweredYes = [];
+let usersAnsweredNo = [];
 app.get("/volunteer/:id", (req, res) => {
-    if(usersAnswered.includes(req.params.id)){
+    if(usersAnsweredYes.includes(req.params.id)){
         res.render("pages/volunteer_dashboard", {data: {userid: req.params.id}});
+    } else if (usersAnsweredNo.includes(req.params.id)){
+        res.render("pages/volunteer_no");
     } else {
         res.render("pages/volunteer_confirm", {data: {userid: req.params.id}});
     }
 });
 
 app.get("/volunteer/:id/yes", (req, res) => {
-    res.render("pages/volunteer_dashboard");
-    usersAnswered.push(req.params.id);
+    res.render("pages/volunteer_dashboard", {data: {userid: req.params.id}});
+    usersAnsweredYes.push(req.params.id);
 });
 
 app.get("/volunteer/:id/no", (req, res) => {
-    res.render("pages/volunteer_no");
-    usersAnswered.push(req.params.id);
+    res.render("pages/volunteer_no", {data: {userid: req.params.id}});
+    usersAnsweredNo.push(req.params.id);
 });
+
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
 });
